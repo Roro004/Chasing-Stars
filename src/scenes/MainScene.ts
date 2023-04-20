d8685f078a04cdf9eb65188981581dff56d711eb
Ali Karbassi <ali@karbassi.com>
2023-04-20 15:37:07 -0500

    Files

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -7,6 +7,26 @@ export default class MainScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, 'pointy')
+
+    /*
+
+    pointy-[speed]-[turn].png
+
+    speed = 0 -> idle; 12 -> full speed
+    turn = 0 -> no turn; 12 -> full turn
+
+    pointy-0-0.png - 23
+    pointy-1-0.png - 24
+    ...
+    pointy-12-0.png - 35
+
+    pointy-0-1.png - 36
+    ...
+    pointy-0-12.png - 180
+
+    ...
+    pointy-12-12.png - 192
+    */
   }
 
   update() {}
